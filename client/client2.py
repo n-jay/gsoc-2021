c77e95098fca81aa97d2d65149efb87cdc3dac8d
n-jay <nuwanjayawardene@gmail.com>
2021-07-17 12:11:16 +0530

    Update client destination endpoint to k8s service

--- client/client2.py
+++ client/client2.py
@@ -4,7 +4,7 @@ from time import time, sleep
 print("Sending request...");
 
 def sendRequest():
-    r = requests.get('http://server:3000');
+    r = requests.get('http://synapse-service:8280/backend');
     print(r.status_code);
     print(r.text);
 
